74b1cfecc7a569d27232f270a4c03476d9e36fa7
Handsol <imhandsol@gmail.com>
2025-01-14 00:33:15 +0900

    fix : 검색 API로 검색 기능 구현
    검색어를 입력하지 않으면 초기 화면으로 돌아가는 명령어 추가
    검색 API를 구현하여 검색어 입력 시 검색어 관련한 영화만 카드UI로 뜨는 부분 확인 완료
    #1

--- api.js
+++ api.js
@@ -10,8 +10,6 @@
 
 // -----------------------------------------------------------------------
 
-let searchData = [];
-
 // API로 fetch 내용 가져오고 json으로 변환하는 부분
 function fetchMovie() {
   fetch(
@@ -52,7 +50,7 @@ function displayMovies(movies) {
     let voteAverage = Math.round(vote_average * 10) / 10;
     const voteElement = document.createElement("p");
     voteElement.classList.add("movieVotes");
-    voteElement.textContent = `평점 : ${voteAverage} (${vote_count})`;
+    voteElement.textContent = `⭐ ${voteAverage} (${vote_count})`;
 
     // 포스터, 제목, 평점을 카드에 추가
     card.appendChild(img);
@@ -67,38 +65,32 @@ function displayMovies(movies) {
 // fetchMovie를 실행
 fetchMovie();
 
-// featchData를 통해 불러온 데이터 중에서 filter를 통해 검색결과 구현
-searchBtn.addEventListener("click", function () {
-  const keyword = searchInput.value;
-  const searchedMovies = searchData.filter(function (movie) {
-    return movie.title.includes(keyword);
-  });
-  console.log("잘 검색됐니? ", searchedMovies);
-  displayMovies(searchedMovies);
-});
-
 // -----------------------------------------------------------------------
+// 검색 결과를 키워드를 통해 구현
 
-// 검색을 위해 접근할 수 있도록 데이터를 배열로 선언.
+let searchData = [];
 
-// 검색을 위한 데이터 불러오고, 배열로 나갈 수 있도록 설정.
-// fetch(
-//   "https://api.themoviedb.org/3/search/movie?query=${title}&include_adult=false&language=ko-KR&page=1",
-//   options
-// )
-//   .then((resSearch) => resSearch.json())
-//   .then((searchDatas) => {
-//     searchData = searchDatas.data;
-//     console.log("잘 검색됐니? ", searchDatas);
-//   })
-//   .catch((errSea) => console.error(errSea));
+searchBtn.addEventListener("click", function () {
+  // keyword 변수를 생성하여 검색창에 입력되는 값 불러오기
+  const keyword = searchInput.value.trim();
 
-// 검색 버튼이 검색창에 들어가는 내용을 가져오도록 설정.
-// searchBtn.addEventListener("click", function () {
-//   const keyword = searchInput.value;
-//   const searchedMovies = searchData.filter(function (movie) {
-//     return movie.title.includes(keyword);
-//   });
-//   console.log("잘 검색됐니? ", searchedMovies);
-//   displayMovies(searchedMovies);
-// });
+  // 검색어가 입력되지 않으면 초기화면으로 돌아가게 세팅
+  if (!keyword) {
+    fetchMovie();
+    return;
+  }
+
+  // 창고(searchAPI)에서 검색어를 기준으로 검색되게 내용 추가
+  const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=false&language=ko-KR&page=1`;
+
+  // 창고 연결, 검색된 결과가 나오도록 변수 연결
+  fetch(searchUrl, options)
+    .then((resSearch) => resSearch.json())
+    .then((data) => {
+      const searchedMovies = data.results;
+
+      // 검색 결과가 화면에 나오도록 설정
+      displayMovies(searchedMovies);
+    })
+    .catch((errSea) => console.error(errSea));
+});
