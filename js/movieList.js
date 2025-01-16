81755ac8afa21fd2d9f1a6e7c11d93b913d73ac6
Handsol <imhandsol@gmail.com>
2025-01-16 22:49:56 +0900

    style : movieList 스타일링
    - 영화 카드가 조금 더 정돈돼보이도록 스타일링
    - 화면 사이즈가 바뀌어도 한 줄에 4개가 유지되도록 조정
    - 무비컨테이너 색상 변
    - 카드에 마우스를 올리면 영화제목이 뜨도록 hover 스타일 추가
    #6

--- js/movieList.js
+++ js/movieList.js
@@ -2,22 +2,27 @@ function displayMovies(movies) {
   // 무비컨테이너 한번 비워줘야함 //
   movieContainer.innerHTML = "";
   for (let i = 0; i < movies.length; i++) {
-    const { id, poster_path, title } = movies[i];
+    const { id, backdrop_path, title } = movies[i];
 
     // 새로운 카드 요소 생성
     const card = document.createElement("div");
     card.classList.add("movieCard");
     card.id = `movie${id}`;
-    card.style.backgroundSize = "cover";
 
     // 포스터 이미지 생성
     const img = document.createElement("img");
     img.classList.add("moviePoster");
-    img.src = `https://image.tmdb.org/t/p/w500${poster_path}`;
+    img.src = `https://image.tmdb.org/t/p/w780${backdrop_path}`;
     img.alt = `${title} 유명포스터`;
 
-    // 포스터, 제목, 평점을 카드에 추가
+    // 영화 제목 생성
+    const movieTitle = document.createElement("div");
+    movieTitle.classList.add("movieTitle");
+    movieTitle.textContent = title;
+
+    // 포스터와 제목을 카드에 추가
     card.appendChild(img);
+    card.appendChild(movieTitle);
 
     // 카드를 컨테이너에 추가
     movieContainer.appendChild(card);
